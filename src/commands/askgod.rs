93555167877ec3c0e4173a60a245fb7f83a6985b
jollycurv-e <258888886+jollycurv-e@users.noreply.github.com>
2026-06-15 21:56:12 -0600

    !askgod added iammovement corpus

--- src/commands/askgod.rs
+++ src/commands/askgod.rs
@@ -55,7 +55,7 @@ fn listgods(ctx: CommandContext<'_>) -> CommandFuture<'_> {
             "moon", "noi", "sophia", "eddy", "krishna", "buddha", "waheguru", "tao",
             "confucius", "amaterasu", "caodai", "zoroaster", "osiris", "odin", "zeus",
             "hurakan", "hammurabi", "huitzilopochtli", "hermetic", "crowley", "eris",
-            "kardec", "tenrikyo", "falun", "rael", "vivec", "dobbs", "bokonon", "tolkien", "shaker", "swedenborg", "canaan", "moorish", "setian", "urantia", "heavensgate", "process", "andraste", "orpheus", "plotinus", "zohar", "sumerian", "lavey", "cathar", "caine", "olamina", "mahavira", "pariacaca", "iching", "kebra", "rasta", "jedi", "qumran", "enoch", "acim", "faithism", "aquarian", "lawofone",
+            "kardec", "tenrikyo", "falun", "rael", "vivec", "dobbs", "bokonon", "tolkien", "shaker", "swedenborg", "canaan", "moorish", "setian", "urantia", "heavensgate", "process", "andraste", "orpheus", "plotinus", "zohar", "sumerian", "lavey", "cathar", "caine", "olamina", "mahavira", "pariacaca", "iching", "kebra", "rasta", "jedi", "qumran", "enoch", "acim", "faithism", "aquarian", "lawofone", "iammovement",
         ];
         const MAX: usize = 220;
         let mut line = format!("!askgod <god> -- {} corpora, one per corpus: ", GODS.len());
@@ -158,10 +158,11 @@ static MANDAEAN_MERGED_CORPUS: OnceLock<Vec<Verse>> = OnceLock::new();
 static FAITHISM_CORPUS: OnceLock<Vec<Verse>> = OnceLock::new();
 static AQUARIAN_CORPUS: OnceLock<Vec<Verse>> = OnceLock::new();
 static LAWOFONE_CORPUS: OnceLock<Vec<Verse>> = OnceLock::new();
+static IAMMOVEMENT_CORPUS: OnceLock<Vec<Verse>> = OnceLock::new();
 
 type CorpusEntry = (&'static OnceLock<Vec<Verse>>, &'static str, fn(&str) -> anyhow::Result<Vec<Verse>>);
 
-fn all_corpora() -> [CorpusEntry; 64] {
+fn all_corpora() -> [CorpusEntry; 65] {
     [
         (&KJV_CORPUS, "godtexts/kjv.txt.zst", parse_kjv),
         (&KORAN_CORPUS, "godtexts/koran.txt.zst", parse_koran),
@@ -227,6 +228,7 @@ fn all_corpora() -> [CorpusEntry; 64] {
         (&FAITHISM_CORPUS, "godtexts/faithism.txt.zst", parse_bahai),
         (&AQUARIAN_CORPUS, "godtexts/aquarian.txt.zst", parse_bahai),
         (&LAWOFONE_CORPUS, "godtexts/lawofone.txt.zst", parse_bahai),
+        (&IAMMOVEMENT_CORPUS, "godtexts/iammovement.txt.zst", parse_bahai),
     ]
 }
 
@@ -486,6 +488,9 @@ pub fn execute(ctx: CommandContext<'_>) -> CommandFuture<'_> {
                 }
                 Some("lawofone") | Some("ramaterial") | Some("confederation") | Some("densities") | Some("wanderer") | Some("harvest") | Some("larussell") | Some("donelkins") | Some("carlarueckert") | Some("racontact") => {
                     (&LAWOFONE_CORPUS, "godtexts/lawofone.txt.zst", parse_bahai)
+                }
+                Some("iammovement") | Some("iam") | Some("saintgermain") | Some("stgermain") | Some("godfreking") | Some("guyballard") | Some("ballard") | Some("lotusray") | Some("ascendedmaster") | Some("mightyiampresence") => {
+                    (&IAMMOVEMENT_CORPUS, "godtexts/iammovement.txt.zst", parse_bahai)
                 }
                 Some("bible") | Some("god") | Some("jesus") | Some("christ") | Some("kjv") | Some("christian") => {
                     (&KJV_CORPUS, "godtexts/kjv.txt.zst", parse_kjv)
